442c92a65bf4ab69b235ba01430f89410cdb4665
Dekun Zhang <91729508+DekunZhang@users.noreply.github.com>
2023-02-25 17:18:27 +0000

    docs: change encoding

--- BLE_server_multiconnect/BLE_server_multiconnect.ino
+++ BLE_server_multiconnect/BLE_server_multiconnect.ino
@@ -1,24 +1,3 @@
-/*
-    Video: https://www.youtube.com/watch?v=oCMOYS71NIU
-    Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleNotify.cpp
-    Ported to Arduino ESP32 by Evandro Copercini
-    updated by chegewara
-
-   Create a BLE server that, once we receive a connection, will send periodic notifications.
-   The service advertises itself as: 4fafc201-1fb5-459e-8fcc-c5c9c331914b
-   And has a characteristic of: beb5483e-36e1-4688-b7f5-ea07361b26a8
-
-   The design of creating the BLE server is:
-   1. Create a BLE Server
-   2. Create a BLE Service
-   3. Create a BLE Characteristic on the Service
-   4. Create a BLE Descriptor on the characteristic
-   5. Start the service.
-   6. Start advertising.
-
-   A connect hander associated with the server starts a background task that performs notification
-   every couple of seconds.
-*/
 #include <BLEDevice.h>
 #include <BLEServer.h>
 #include <BLEUtils.h>
@@ -28,14 +7,16 @@ BLEServer* pServer = NULL;
 BLECharacteristic* pCharacteristic = NULL;
 bool deviceConnected = false;
 bool oldDeviceConnected = false;
-uint32_t value = 0;
-
-// See the following for generating UUIDs:
-// https://www.uuidgenerator.net/
 
 #define SERVICE_UUID        "02afd1d9-f889-4f49-acc7-33b34a620adb"
 #define CHARACTERISTIC_UUID "3604da64-2145-422a-89c6-c540392c3a6a"
 
+struct PayloadPacket {
+  uint32_t timestamp;
+  uint16_t data;
+};
+
+std::string encode_packet(PayloadPacket packet);
 
 class MyServerCallbacks: public BLEServerCallbacks {
     void onConnect(BLEServer* pServer) {
@@ -51,7 +32,6 @@ class MyServerCallbacks: public BLEServerCallbacks {
 class MyCallbacks: public BLECharacteristicCallbacks {
     void onWrite(BLECharacteristic *pCharacteristic) {
       std::string value = pCharacteristic->getValue();
-
       if (value.length() > 0) {
         Serial.println("*********");
         Serial.print("New value: ");
@@ -108,10 +88,10 @@ void setup() {
 void loop() {
     // notify changed value
     if (deviceConnected) {
-        pCharacteristic->setValue((uint8_t*)&value, 4);
+        PayloadPacket packet = { 0xDEADBEEF, 0xB00F};
+        std::string data = encode_packet(packet);
+        pCharacteristic->setValue(data);
         pCharacteristic->notify();
-        value++;
-        delay(100); // bluetooth stack will go into congestion, if too many packets are sent, in 6 hours test i was able to go as low as 3ms
     }
     // disconnecting
     if (!deviceConnected && oldDeviceConnected) {
@@ -125,4 +105,9 @@ void loop() {
         // do stuff here on connecting
         oldDeviceConnected = deviceConnected;
     }
+    delay(100); // bluetooth stack will go into congestion, if too many packets are sent, in 6 hours test i was able to go as low as 3ms
+}
+
+std::string encode_packet(PayloadPacket packet) {
+  return std::to_string(packet.timestamp) + '#' + std::to_string(packet.data);
 }
